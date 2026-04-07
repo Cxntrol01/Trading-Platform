dbf85243e138d523be3884dd5952845768cc3910
Cxntrol01 <102139461+Cxntrol01@users.noreply.github.com>
2026-04-07 20:37:33 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import FullscreenChart from "@/components/FullscreenChart";
+
+export default function Home() {
+  return (
+    <main className="w-full h-screen bg-black text-white">
+      <FullscreenChart />
+    </main>
+  );
+}
